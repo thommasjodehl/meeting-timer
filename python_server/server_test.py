cc9d8f1d8e31060115ec78824e85490d5f4c8b7a
thommasjodehl <thommas_jodehl@hotmail.com>
2016-06-12 18:12:17 +0200

    Implemented timeout handling.
    Implemented settimeout() on the test server. The script now also handles
    if there is no one to connect to.

--- python_server/server_test.py
+++ python_server/server_test.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 #create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,14 +10,20 @@ serversocket.bind(('localhost', 8888))
 
 serversocket.listen(1)
 
-while 1:
-   #accept connections from outside
+# Set the timeout of the socket to 250 miliseconds.
+serversocket.settimeout(0.250)
+
+client = 0
+
+try: # try to accept a connection.
    print ("Waiting for someone to connect..")
    (client, address) = serversocket.accept()
-   print ("New connection from", address)
-   client.setblocking(0)
-   if client != 0:
-      print("Someone connected..")
+   if client == 0: # if it was not possible to connect to a client
+      print("No one to connect to..")
+      serversocket.close()
+      sys.exit(1)
+   else: # else do the business
+      print ("New connection from", address)
       msg = client.recv(14).decode()
       if msg != 0:
          print ("Received message..")
@@ -24,3 +31,6 @@ while 1:
          msg = "Hello client!"
          client.sendall(msg.encode())
          client.close()
+         serversocket.close()
+except OSError as msg: # handle when there is no one to connect to.
+   serversocket.close()
